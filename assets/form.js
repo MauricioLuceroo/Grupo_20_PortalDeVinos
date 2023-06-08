a3187e7a5d6d57f3cd1ca13a17e9aea2efaa8e6c
MauricioLuceroo <maurilucero31@gmail.comP>
2023-06-08 02:34:22 -0300

    commit 2

---
+++ assets/form.js
@@ -0,0 +1,47 @@
+var imput = document.getElementsByClassName("formulario_imput");
+for (var i =0 ; i < imput.length; i++) {
+    imput[i].addEventListener("keyup", function(){
+        if(this.value.length>=1){
+            this.nextElementSibling.classList.add("fijar");
+        }else{
+            this.nextElementSibling.classList.remove("fijar");
+        }
+        
+    });
+}
+
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+  
+    var name = document.getElementsByName("name")[0].value;
+    var email = document.getElementsByName("email")[0].value;
+    var subject = document.getElementsByName("Subject")[0].value;
+  
+    if (name === "") {
+      alert("Por favor, ingresa tu Nombre");
+      return;
+    }
+  
+    if (email === "") {
+      alert("Por favor, ingresa tu Correo electrónico");
+      return;
+    }
+  
+    if (subject === "") {
+      alert("Por favor, ingresa el Asunto");
+      return;
+    } 
+  
+    alert("El formulario se ha enviado correctamente");
+    
+  
+    document.getElementById("form").reset();
+  });
+ 
+
+ 
+  
+  
+  
+  
+  
---
+++ readme.md
@@ -0,0 +1,36 @@
+## Autor
+
+Mauricio Lucero Franco
+
+## Github repo
+
+Repositorio: [link](https://github.com/codoacodo-22539/ejemplo-tpo.git) 
+
+## Sitio Web
+
+Netlify: [link](https://german-ejemplo-tpo.netlify.app/)
+
+## Tech Stack
+
+**Client:** HTML, CSS, Vanilla JavaSCript
+
+## Tools Utilizadas
+
+- (https://fonts.googleapis.com)
+- FontAwesome
+
+## Transicion
+
+Se incorporo una transicion en:
+-Index 
+-Tintos.html
+-Blancos.html
+
+
+## Estructura de carpeta
+
+El index.html, un archivo readme y un screenshot son los archivos en el root folder.
+
+La carpeta assets contiene css styles, icons, imagenes y los scripts.
+
+La carpeta pages contiene los htmls de las paginas.
